07c0b953a2e39447d7b32f1be73b9559a1c54e63
AngryNeighbour <130363794+AngryNeighbour@users.noreply.github.com>
2023-04-18 20:52:46 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    document.getElementById("demo").style.fontSize = "55px";
+    document.getElementById("demo").style.backgroundColor = "red";
+    document.getElementById("demo").style.color = "rgb(255,0,255)";
+    document.getElementById("demo").style.border = "2px dashed black"
+}
